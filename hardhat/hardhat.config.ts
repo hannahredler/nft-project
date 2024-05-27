c46b6d86a8cec348b3562828a3cd14c37a091e09
Hannah Redler <hannahjredler@gmail.com>
2024-05-27 19:52:38 +0200

    configure sepolia testnet

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -1,8 +1,18 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+const fs = require("fs");
+const SEPOLIA_ACCOUNT_KEY = fs.readFileSync(".secret").toString().trim();
+const INFURA_API_KEY = fs.readFileSync(".infura").toString().trim();
+
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [SEPOLIA_ACCOUNT_KEY],
+    },
+  },
 };
 
 export default config;
