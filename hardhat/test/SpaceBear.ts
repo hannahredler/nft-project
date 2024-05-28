c730f3f8f15954c4d37506fc275108c7292fb4b0
Hannah Redler <hannahjredler@gmail.com>
2024-05-28 10:25:05 +0200

    Add load fixture for spacebear deploy and mint

--- hardhat/test/SpaceBear.ts
+++ hardhat/test/SpaceBear.ts
@@ -1,27 +1,37 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
 
 describe("SpaceBear", () => {
-  it("is possible to mint a token", async () => {
+  async function deploySpaceBearAndMintTokenFixture() {
     const SpaceBear = await ethers.getContractFactory("SpaceBear");
     const spaceBearInstance = await SpaceBear.deploy();
 
     const [owner, otherAccount] = await ethers.getSigners();
-    await spaceBearInstance.safeMint(otherAccount.address, "spacebear_1.json");
+    const transaction = await spaceBearInstance.safeMint(
+      otherAccount.address,
+      "spacebear_1.json"
+    );
+
+    return { spaceBearInstance, transaction };
+  }
+  it("is possible to mint a token", async () => {
+    const { spaceBearInstance } = await loadFixture(
+      deploySpaceBearAndMintTokenFixture
+    );
+    const [owner, otherAccount] = await ethers.getSigners();
     expect(await spaceBearInstance.ownerOf(0)).to.equal(otherAccount.address);
   });
 
   it("fails to transfer tokens from the wrong address", async () => {
     const SpaceBear = await ethers.getContractFactory("SpaceBear");
-    const spaceBearInstance = await SpaceBear.deploy();
+    const { spaceBearInstance, transaction } = await loadFixture(
+      deploySpaceBearAndMintTokenFixture
+    );
     const [owner, otherAccount, notTheNftOwner] = await ethers.getSigners();
 
-    const transaction = await spaceBearInstance.safeMint(
-      otherAccount,
-      "spacebear_1.json"
-    );
-
     const receipt = await transaction.wait();
+
     expect(await spaceBearInstance.ownerOf(0)).to.equal(otherAccount.address);
 
     await expect(
